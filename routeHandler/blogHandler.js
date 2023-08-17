7a535c29fdabcc50a1f550afb04ee8591ac15344
OmarFaruq967 <rone967@gmail.com>
2023-08-17 23:18:58 +0600

    Route Handeler added

--- routeHandler/blogHandler.js
+++ routeHandler/blogHandler.js
@@ -4,10 +4,41 @@ const router = express.Router();
 const blogSchema = require('../schemas/blogSchema');
 const Blog = new mongoose.model("Blog", blogSchema);
 
-// get all todos
+// get all Blogs
 router.get('/', async (req, res) => {
-    console.log('This is home page for server site sayeed');
+    await Blog.find().then((data) => {
+        console.log(data);
+             res.json({
+                 result: data,
+                 message: 'success'
+             })
+         
+     }).catch(err => {
+         console.log(err);
+         res.json({
+             message: "error"
+         })
+   
+     })
 })
+
+
+router.post('/', async (req, res) => {
+    const newBlog = new Feedback(req.body);
+    await newBlog.save().then((data) => {
+
+        res.status(200).json({
+            
+            message: 'success'
+        })
+    }).catch((err) => {
+        if(err){
+            console.log(err);
+            res.status(500).json({
+                message: 'error'
+        })}})
+});
+
 
 
 module.exports = router;
