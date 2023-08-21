764858ce50dc9ae59a5fd93b6477894eccbbfecb
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-08-21 20:48:04 +0600

    first commit

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -8,29 +8,7 @@ const User = new mongoose.model("User", userSchema);
 router.get("/", async (req, res) => {
   console.log("This is home page for server site");
 });
-// get all todos
-// router.post("/", async (req, res) => {
-//   console.log("This is home page for server site");
-// });
-router.post("/", async (req, res) => {
-  const user = req.body;
-  console.log(user);
 
-  try {
-    const existingUser = await User.findOne({ email: user.email });
-
-    if (existingUser) {
-      return res.send({ message: "User already exists" });
-    }
-
-    const newUser = new User(user);
-    const result = await newUser.save();
-    res.send(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: "Error creating user" });
-  }
-});
 // router.post("/users", async (req, res) => {
 //   const user = req.body;
 //   console.log(user);
