618501da7b5acfda32fd96a4b5d61a8d99a040e8
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-08-22 15:02:11 +0600

    set user role

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,12 +1,25 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
+const userSchema = new mongoose.Schema({
+  username: {
     type: String,
+    unique: false,
   },
   email: {
     type: String,
+    required: true,
+    unique: true,
   },
+  userRole: String,
+  gender: String,
+  address: String,
+  socialLinks: Array,
+  website: String,
+  mobile: Number,
+  dateOfBirth: Date,
+  education: Array,
+  skills: Array,
+  userPhotoUrl: String,
 });
 
 module.exports = userSchema;
