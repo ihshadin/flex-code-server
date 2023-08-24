7719877a1f3c7fce1bd92393a58807d5de0245fa
OmarFaruq967 <rone967@gmail.com>
2023-08-25 00:29:24 +0600

    Add Problem route added

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // Routes function
-const flexHandler = require('./routeHandler/flexHandler')
-const blogHandler = require('./routeHandler/blogHandler')
-const userHandler = require('./routeHandler/userHandler')
-const feedbackHandler = require('./routeHandler/feedbackHandler')
+const flexHandler = require("./routeHandler/flexHandler");
+const blogHandler = require("./routeHandler/blogHandler");
+const userHandler = require("./routeHandler/userHandler");
+const feedbackHandler = require("./routeHandler/feedbackHandler");
+const problemHandler = require("./routeHandler/problemHandler");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-const dburi = `mongodb+srv://${process.env.FLEXCODE_USERNAME}:${process.env.FLEXCODE_PASSWORD}@cluster0.f5zl9xv.mongodb.net/flexCodeDB?retryWrites=true&w=majority`
+const dburi = `mongodb+srv://${process.env.FLEXCODE_USERNAME}:${process.env.FLEXCODE_PASSWORD}@cluster0.f5zl9xv.mongodb.net/flexCodeDB?retryWrites=true&w=majority`;
 
 const databaseConnect = async () => {
-    try {
-       await  mongoose.connect(dburi, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // serverSelectionTimeoutMS: 30000,
-    })
-        console.log('Database connection successful');
-    } catch (error) {
-        console.log(error.message);
-        console.log('Database connection failed');
-    }
-}
+  try {
+    await mongoose.connect(dburi, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // serverSelectionTimeoutMS: 30000,
+    });
+    console.log("Database connection successful");
+  } catch (error) {
+    console.log(error.message);
+    console.log("Database connection failed");
+  }
+};
 
 // application routes
-app.use('/problems', flexHandler);
-app.use('/blog', blogHandler);
-app.use('/users', userHandler);
-app.use('/feedback', feedbackHandler);
+app.use("/problems", flexHandler);
+app.use("/blog", blogHandler);
+app.use("/users", userHandler);
+app.use("/feedback", feedbackHandler);
+app.use("/problem", problemHandler);
 
-app.get('/', (req, res) => {
-    res.send('FlexCode. Unlock your code knowledge');
-    databaseConnect()
-})
+databaseConnect();
+
+app.get("/", (req, res) => {
+  res.send("FlexCode. Unlock your code knowledge");
+});
 app.listen(port, (req, res) => {
-    console.log('FlexCode are runnin on: ', port);
-})
-
+  console.log("FlexCode are runnin on: ", port);
+});
