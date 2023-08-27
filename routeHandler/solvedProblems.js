3e8fb0a794dfb4a472337a2a82dadb44445a455e
Imam Hossain <shadinfr@yahoo.com>
2023-08-27 22:26:54 +0600

    dashboard data dynamic

--- routeHandler/solvedProblems.js
+++ routeHandler/solvedProblems.js
@@ -2,20 +2,34 @@ const express = require('express');
 const router = express.Router();
 const solvedProblemsSchema = require('../schemas/solvedProblemSchema');
 const { mongoose } = require('mongoose');
-const SolvedProblems = new mongoose.model("SolvedProblems", solvedProblemsSchema)
-
+const SolvedProblem = new mongoose.model("SolvedProblem", solvedProblemsSchema)
 
 router.get('/', async (req, res) => {
-    res.send("imam vai is running")
+    try {
+        const solvedProblems = await SolvedProblem.find();
+        res.json(solvedProblems);
+    } catch (error) {
+        res.status(500).send('Server Error');
+    }
+})
+
+router.get('/userSolveProblem', async (req, res) => {
+    try {
+        const userEmail = req.query.email
+        const solvedProblems = await SolvedProblem.find({ userEmail: userEmail });
+        res.json(solvedProblems);
+    } catch (error) {
+        res.status(500).send('Server Error');
+    }
 })
 
 router.post('/', async (req, res) => {
-    const solvedProblem = new SolvedProblems(req.body)
     try {
+        const solvedProblem = new SolvedProblem(req.body)
         await solvedProblem.save()
-        res.status(200).json({message: "success"})
+        res.status(200).json({ message: "success" })
     } catch (error) {
-        res.status(500).json({message: "error"})
+        res.status(500).json({ message: "error" })
     }
 })
 
