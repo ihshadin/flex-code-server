6afa6aadceec8bc18c1c920ee0101da5b91c3425
Imam Hossain <shadinfr@yahoo.com>
2023-08-28 22:45:29 +0600

    notebook er backend improve

---
+++ routeHandler/noteBookHandler.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const noteBookSchema = require('../schemas/noteBookSchema');
+const NoteBook = new mongoose.model("noteBook", noteBookSchema);
+
+// get all NoteBook
+router.get("/", async (req, res) => {
+  try {
+    const NoteBooks = await NoteBook.find();
+    res.json(NoteBooks);
+  } catch (error) {
+    res.status(500).send('Server Error');
+  }
+});
+
+// Get Single Note details
+router.get("/:id", async (req, res) => {
+  try {
+    const NoteBookId = req.params.id;
+    const noteBook = await NoteBook.findById(NoteBookId);
+    if (!noteBook) {
+      return res.status(404).json({
+        message: "NoteBook not found!",
+      });
+    }
+    res.json(noteBook);
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
+
+// Create a new Note
+router.post("/", async (req, res) => {
+  try {
+    const newNote = new NoteBook(req.body);
+    await newNote.save();
+    res.status(200).json({
+      message: "success",
+    });
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
--- schemas/noteBookSchema.js
+++ schemas/noteBookSchema.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const nodeBookSchema = mongoose.Schema({
-    
-        name: String,
+        title: String,
         details: String,
 })
 
