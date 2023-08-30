15e1a9e868e2d3453720afbb14b3891be9dcee95
Imam Hossain <shadinfr@yahoo.com>
2023-08-30 13:23:27 +0600

    leaderbpard api create

--- routeHandler/solvedProblems.js
+++ routeHandler/solvedProblems.js
@@ -23,6 +23,51 @@ router.get('/userSolveProblem', async (req, res) => {
     }
 })
 
+// Data for leaderboard
+router.get('/leaderboard', async (req, res) => {
+    try {
+        const leaderboardData = await SolvedProblem.aggregate([
+            {
+                $lookup: {
+                    from: 'students', // Name of the users collection
+                    localField: 'userEmail',
+                    foreignField: 'email',
+                    as: 'userData',
+                },
+            },
+            {
+                $unwind: '$userData',
+            },
+            // {
+            //     $project: {
+            //         userName: '$userData.username',
+            //         // userPhoto: '$userData.photo',
+            //         userEmail: 1,
+            //         points: 1,
+            //     },
+            // },
+            {
+                $group: {
+                    _id: '$userEmail',
+                    userName: { $first: '$userData.username' },
+                    // userPhoto: { $first: '$userData.userPhoto' },
+                    userEmail: { $first: '$userEmail' },
+                    points: { $sum: '$points' },
+                    problemsSolved: { $sum: 1 }, // Count of solved problems
+                },
+            },
+            {
+                $sort: { points: -1 },
+            },
+        ]);
+
+        res.json(leaderboardData);
+    } catch (error) {
+        console.error('Error fetching leaderboard data:', error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
         const solvedProblem = new SolvedProblem(req.body)
