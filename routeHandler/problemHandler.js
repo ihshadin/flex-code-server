beeb59297581e67a8efc9a96f254e46aa0511564
Imam Hossain <shadinfr@yahoo.com>
2023-09-01 01:01:33 +0600

    problem fix

--- routeHandler/problemHandler.js
+++ routeHandler/problemHandler.js
@@ -6,39 +6,58 @@ const Problem = new mongoose.model("Problem", problemSchema);
 
 // Get all problems 
 router.get("/", async (req, res) => {
-    try {
-    const problemLevel=req.query.level
-    if (!problemLevel) {
-      const data = await Problem.find();
-      res.json(data);
-    } else {
-      const data = await Problem.find({ level: problemLevel });
-      res.json(data);
+  try {
+    const searchText = req.query.search;
+    const problemLevel = req.query.level;
+    let query = {};
+    if (problemLevel) {
+      query.level = problemLevel;
     }
-    } catch (err) {
-      res.status(500).json({
-        message: "error",
-      });
+    if (searchText) {
+      query.title = { $regex: searchText, $options: "i" };
     }
+    const data = await Problem.find(query);
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({
+      message: "error",
+    });
+  }
 });
 
-
+// Get Single problem details
+router.get("/:id", async (req, res) => {
+  const problemId = req.params.id;
+  try {
+    const problem = await Problem.findById(problemId);
+    if (!problem) {
+      return res.status(404).json({
+        message: "Problem not found",
+      });
+    }
+    res.json(problem);
+  } catch (err) {
+    res.status(500).json({
+      message: "error",
+    });
+  }
+});
 
 
 // Create a new Add problem solving code
 router.post("/", async (req, res) => {
-    const newProblem = new Problem(req.body);
-    try {
-      await newProblem.save();
-      res.status(200).json({
-        message: "success",
-      });
-    } catch (err) {
-      res.status(500).json({
-        message: "error",
-      });
-    }
-  });
+  const newProblem = new Problem(req.body);
+  try {
+    await newProblem.save();
+    res.status(200).json({
+      message: "success",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "error",
+    });
+  }
+});
 
 
 
