0b03e51af734c3bdf100d262e81fec9611bd9e40
jahid <jahidmorol2@gmail.com>
2023-09-19 16:11:34 +0600

    add something in challenges schema

--- schemas/challengeSchema.js
+++ schemas/challengeSchema.js
@@ -1,35 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const challengeSchema = new mongoose.Schema({
   senderName: {
-      type: String
-    },
-    sender: {
-      type: String
-    },
-    senderImg: {
-      type: String
-    },
-    receiver: {
-      type: String
-    },
-    winner: {
-      type: String,
-    },
-    winnerTime: {
-      type: Number,
-    },
-    problem: {
-      type: String,
-    },
-    problemId: {
-      type: mongoose.Schema.Types.ObjectId,
-    },
-    timeStamp: {
-      type: Number,
-    },
-    date: { type: Date, default: Date.now }
-  });
+    type: String,
+  },
+  sender: {
+    type: String,
+  },
+  senderImg: {
+    type: String,
+  },
+  receiver: {
+    type: String,
+  },
+  receiverImg: {
+    type: String,
+  },
+  receiverName: {
+    type: String,
+  },
+  winner: {
+    type: String,
+  },
+  winnerTime: {
+    type: Number,
+  },
+  problem: {
+    type: String,
+  },
+  problemId: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  timeStamp: {
+    type: Number,
+  },
+  date: { type: Date, default: Date.now },
+});
 
-  
 module.exports = challengeSchema;
